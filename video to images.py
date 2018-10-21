99040fbe3b0d40c78bdeef17507b5e63a7f76959
Mohamed Afsar Meera S <35129908+afsarmeera@users.noreply.github.com>
2018-10-21 20:46:15 +0530

    Update video to images.py
    Video to Frame Convertion

---
+++ video to images.py
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+# Program To Read video 
+# and Extract Frames 
+import cv2 
+  
+# Function to extract frames 
+def FrameCapture(path): 
+    
+    # Path to video file 
+    vidObj = cv2.VideoCapture(path) 
+  
+    # Used as counter variable 
+    count = 0
+  
+    # checks whether frames were extracted 
+    success = 1
+  
+    while success: 
+  
+        # vidObj object calls read 
+        # function extract frames 
+        success, image = vidObj.read() 
+        
+        
+       #c = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Saves the frames with frame-count 
+        cv2.imwrite("frame%d.jpg" % count, cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)) 
+  
+        count += 1
+  
+# Driver Code 
+if __name__ == '__main__': 
+  
+    # Calling the function 
+    FrameCapture("file:///C:/Users/afsar/Downloads/videoplayback.mp4") 
